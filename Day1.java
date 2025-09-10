69445d1d3169d564fa9b6c2130f9f2ae389cd754
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-10 13:08:46 +0800

    Hello World
    Program "Hello World!" dan struktur dasar

--- Day1.java
+++ Day1.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package testing;
-
-/**
- *
- * @author Muh.Rayhan.S
- */
 public class Testing {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        //Program "Hello World" dan struktur dasar
+        System.out.println("Hello World!");
         System.out.println("Day 1 of 100 Days Coding");
         System.out.println("Muh.Rayhan.S");
     }
