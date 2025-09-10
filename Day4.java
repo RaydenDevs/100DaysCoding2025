10dbedbc1a0879c899582a19b47b2cfdbdc25b1a
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-11 00:38:55 +0800

    Escape Sequence
    \n \t \b \r \' \" \\

---
+++ Day4.java
@@ -0,0 +1,22 @@
+package OneHundredDaysOfCoding;
+public class Day4 {
+    public static void main(String[] args) {
+        /*  
+        Escape Sequence
+        \n = Newline
+        \t = Tab horizontal
+        \b = Backspace
+        \r = Carriage return
+        \' = Karakter kutip 1/tunggal
+        \" = Karakter kutip 2/ganda
+        \\ = Karakter backslash
+        */  
+        System.out.println("ini contoh newline \nini Newline");
+        System.out.println("ini contoh \tTab");
+        System.out.println("ini contoh backspace, ini\b Backspace");
+        System.out.println("ini contoh \rCarriage Returm");
+        System.out.println("ini contoh \'kutip Tunggal\'");
+        System.out.println("ini contoh \"kutip Ganda\"");
+        System.out.println("ini contoh \\backslash\\");
+    }
+}
