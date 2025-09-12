0e737f81cc4855db4e36319e2a62029335f1a130
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-12 13:15:52 +0800

    Tipe Data Numerik
    byte, short, int, long

---
+++ Day5.java
@@ -0,0 +1,18 @@
+package OneHundredDaysOfCoding;
+public class Day5 {
+    public static void main(String[] args) {
+        //byte 8-bit (1 byte)
+        byte b = 127;
+        //16-bit (2 byte)
+        short s = 32767;
+        //32-bit (4 byte)
+        int i = 2147483647;
+        //64-bit (8 byte)
+        long l = 1234567890;
+        
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+    }
+}
