fd6e83570e3f07e33ddbc1c511fdd86e7afbac98
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-14 23:33:39 +0800

    Tipe Data Char & Boolean
    Char dan Boolean

---
+++ Day7.java
@@ -0,0 +1,18 @@
+package OneHundredDaysOfCoding;
+public class Day7 {
+    public static void main(String[] args) {
+        char huruf = 'A';
+        char angka = '7';
+        char simbol = '#';
+        
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Angka: " + angka);
+        System.out.println("Simbol: " + simbol);
+        
+        System.out.println("Apakah Java mudah? " + benar);
+        System.out.println("Apakah sekarang siang? " + salah);
+    }
+}
