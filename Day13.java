17c8de1fc58dd5ea5c62f9e1b00edb8ca34db1cc
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-20 19:52:58 +0800

    Evaluasi Mentor
    Pertukaran Nilai

---
+++ Day13.java
@@ -0,0 +1,18 @@
+package OneHundredDaysOfCoding;
+public class Day13 {
+    public static void main(String[] args) {
+        char a = ('&');
+        char b = ('$');
+        char c = ('@');
+        char d;
+        
+        System.out.printf("URUTAN NILAI SEBELUM DITUKAR: %c, %c, %c\n\n", a, b, c);
+        
+        d = a;
+        a = b;
+        b = d;
+        //Nilai c masih sama jadi tidak perlu ditukar
+        
+        System.out.printf("URUTAN NILAI SESUDAH DITUKAR: %c, %c, %c\n\n", a, b, c);
+    }
+}
