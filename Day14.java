50064f9483621c5da0a08aa05f22896d1fa3c68e
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-21 16:53:57 +0800

    String ke Tipe data primitif
    String to Char,Integer,Double, and Boolean with Xxxx.parseXxxx()

---
+++ Day14.java
@@ -0,0 +1,24 @@
+package OneHundredDaysOfCoding;
+public class Day14 {
+    public static void main(String[] args) {
+        //String to Char
+        String a = "Ada";
+        char a2 = a.charAt(0);
+        System.out.println(a2);
+        
+        //String to Integer
+        String b = "12";
+        int b2 = Integer.parseInt(b);
+        System.out.println(b2);
+        
+        //String to Double
+        String c = "5.0";
+        double c2 = Double.parseDouble(c);
+        System.out.println(c2);
+        
+        //String to Boolean
+        String d = "true";
+        boolean d2 = Boolean.parseBoolean(d);
+        System.out.println(d2);
+    }
+}
