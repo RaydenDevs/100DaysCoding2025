09ead17e8b79128d4afb023a86e522d617c0e864
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-22 23:24:59 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)
    Penjumlahan (+) dan Pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println("Hasil penjumlahan dari " + a + " + " + b + " = " + c);
+    }
+}
