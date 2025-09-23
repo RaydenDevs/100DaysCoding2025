6b71f5ee67478313022542feff02fc2cc536841e
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-23 23:22:59 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/).
    Perkalian (*) dan Pembagian (/).

---
+++ Day16.java
@@ -0,0 +1,11 @@
+package OneHundredDaysOfCoding;
+public class Day16 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = a * b;
+        int d = a / b;
+        System.out.println("Hasil perkaliam dari " + a + " * " + b + " = " + c);
+        System.out.println("Hasil pembagian dari " + a + " / " + b + " = " + d);
+    }
+}
