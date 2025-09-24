31b6514bd48438985e48ccdab22d7e0e7ffa4956
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-24 09:42:04 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%)
    Modulus atau Sisa Bagi (%)

---
+++ Day17.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day2 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int c = a % b;
+        System.out.println("Sisa bagi dari " + a + " / " + b + " = " + c);
+    }
+}
