f23cc7e83ed36a8f90a6f1be9f774a8f0c29b048
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-25 23:14:18 +0800

    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).
    Tipe Data Kecil ke Besar

---
+++ Day18.java
@@ -0,0 +1,11 @@
+package OneHundredDaysOfCoding;
+public class Day2 {
+    public static void main(String[] args) {
+        char a = 'A';
+        System.out.println("\nChar : "+a);
+        int a2 = a;
+        System.out.println("Hasil konversi otomatis char ke integer : "+a2);
+        double a3 = a2;
+        System.out.println("Hasil konversi otomatis integer ke Double : "+a3);
+    }
+}
