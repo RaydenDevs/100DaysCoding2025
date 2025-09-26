d3e9ea97c12cf6f12bd865a1076bedbd778a6dfe
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-26 21:32:17 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).
    Konversi tipe darta besar ke kecil

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package OneHundredDaysOfCoding;
+public class Day19 {
+    public static void main(String[] args) {
+        double a = 65;
+        System.out.println("Double\t: "+a);
+        int b = (int) a;
+        System.out.println("Integer\t: "+b);
+        char c = (char) b;
+        System.out.println("Char\t: "+c);
+        byte d = (byte) c;
+        System.out.println("Byte\t: "+d);
+    }
+}
