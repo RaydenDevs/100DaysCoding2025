aaa2d2ff73ce7ffd4458e05bc306f6128682d230
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-26 21:35:23 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).
    Konversi tipe data besar ke kecil

--- Day19.java
+++ Day19.java
@@ -1,7 +1,7 @@
 package OneHundredDaysOfCoding;
 public class Day19 {
     public static void main(String[] args) {
-        double a = 65;
+        double a = 65.66;
         System.out.println("Double\t: "+a);
         int b = (int) a;
         System.out.println("Integer\t: "+b);
