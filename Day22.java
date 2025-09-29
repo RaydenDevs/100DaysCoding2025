2060df10b2882443f2f69562483748b4c46c09d9
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-29 23:41:18 +0800

    Menghitung luas persegi
    Panjang x Lebar

--- Day22.java
+++ Day22.java
@@ -2,7 +2,7 @@ package OneHundredDaysOfCoding;
 
 import java.util.Scanner;
 
-public class Day2 {
+public class Day22 {
     public static void main(String[] args) {
         Scanner i = new Scanner(System.in);
         
