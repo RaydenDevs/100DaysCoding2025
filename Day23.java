1bcd3c647d6c4d805a6edc6f052e957e59a7c4ec
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-09-30 21:00:19 +0800

    Program Luas Persegi Panjang.
    Menghitung luas persegi panjang (panjang x lebar)

---
+++ Day23.java
@@ -0,0 +1,18 @@
+package OneHundredDaysOfCoding;
+
+import java.util.Scanner;
+
+public class Day23 {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        
+        System.out.print("Panjang Persegi = ");
+        int p = i.nextInt();
+        
+        System.out.print("lebar Persegi = ");
+        int l = i.nextInt();
+        
+        int ls = p*l;
+        System.out.printf("Sebuah persegi panjang memiliki panjang %d cm dan lebar %d cm, berarti luas persegi panjang adalah %d cm\n", p, l, ls);
+    }
+}
