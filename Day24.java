45069dcad56ebb0adf13501d59c95e66b785dd54
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-01 23:04:18 +0800

    Luas Lingkaran
    PI*Jari2

---
+++ Day24.java
@@ -0,0 +1,18 @@
+package OneHundredDaysOfCoding;
+
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        
+        System.out.print("panjang Jari-jari = ");
+        double r = i.nextDouble();
+        
+        final double pi = 3.14;
+        
+        double l = pi*r*r;
+        
+        System.out.println("Luas Jari-jari = " + l);
+    }
+}
