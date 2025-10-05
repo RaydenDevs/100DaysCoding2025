0c28cd41337577fc0953411f6f7bc76e50fe2301
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-05 22:16:25 +0800

    Operator perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,13 @@
+package OneHundredDaysOfCoding;
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 10;
+        
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a==c);
+        System.out.println(a!=c);
+    }
+}
