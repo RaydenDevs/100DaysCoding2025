bd0d015fd1619ebc4c29790cd276aeb40b23b841
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-09 23:19:52 +0800

    Operator logika ||
    jika salah satu syarat terpenuhi = true

---
+++ Day32.java
@@ -0,0 +1,10 @@
+package OneHundredDaysOfCoding;
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        
+        System.out.println(a>=5 || b>=a); //true
+        System.out.println(a>=15 || b>=15); //false
+    }
+}
