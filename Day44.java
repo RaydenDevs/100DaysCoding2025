cb9e58e72ee6eea0d5ae5ecea1b3a0e569942ebc
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-21 00:18:18 +0800

    Menentukan Predikat
    Updated conditions for grading criteria to be exclusive.

--- Day44.java
+++ Day44.java
@@ -11,11 +11,11 @@ public class Day44 {
         
         if (a >= 93) {
             System.out.println("Predikat A");
-        } else if (a >= 81 && a <= 93) {
+        } else if (a >= 81 && a < 93) {
             System.out.println("Predikat B");
-        } else if (a >= 75 && a <= 81) {
+        } else if (a >= 75 && a < 81) {
             System.out.println("Predikat C");
-        } else if (a >= 60 && a <= 75) {
+        } else if (a >= 60 && a < 75) {
             System.out.println("Predikat D");
         } else {
             System.out.println("Predikat E");
