b40633111b974103ae142c69a94f35185f67cbcc
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-26 23:32:41 +0800

    Operator Ternary

---
+++ Day49.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day49 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a>b ? "a lebih besar" : "b lebih besar");
+        System.out.println((a>b) ? a-b : a+b);
+    }
+}
