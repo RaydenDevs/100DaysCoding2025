987d59e6e7b59272bfe9b8ecdb377e6fac5cd903
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-29 20:50:33 +0800

    Perulangan (While)

---
+++ Day52.java
@@ -0,0 +1,10 @@
+package OneHundredDaysOfCoding;
+public class Day52 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a<=5){
+            System.out.print("["+a+"] ");
+            a++;
+        }
+    }
+}
