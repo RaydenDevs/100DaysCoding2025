02a3daa404062bcd1439fb40d9c7b7644a1bd70c
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-30 23:18:52 +0800

    Break keyword
    menghentikan loop/switch

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package OneHundredDaysOfCoding;
+public class Day53 {
+    public static void main(String[] args) {
+        // break digunakan untuk menghentikan loop/switch
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            if (i == 5){
+                break;
+            }
+        }
+    }
+}
