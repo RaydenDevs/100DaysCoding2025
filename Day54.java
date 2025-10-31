b515f4a011b58acd5ad788ec2413f7b01ce5e678
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-10-31 21:03:26 +0800

    Continue keyword
    skip loop

---
+++ Day54.java
@@ -0,0 +1,12 @@
+package OneHundredDaysOfCoding;
+public class Day54 {
+    public static void main(String[] args) {
+        // continue digunakan untuk skip 1 langkah
+        for (int i = 0; i < 10; i++) {
+            if (i == 3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
