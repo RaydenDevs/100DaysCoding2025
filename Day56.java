9585610e56d2a6e8a318bb9dedcbcaa20b1ec571
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-11-02 23:16:01 +0800

    Perulangan do-while

---
+++ Day56.java
@@ -0,0 +1,10 @@
+package OneHundredDaysOfCoding;
+public class Day56 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Perulangan ke: "+i);
+            i++;
+        } while (i <= 5);
+    }
+}
