5e9e0d39a592fcb7decdcd2ac9bd7d27c0bf1f42
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-11-12 22:47:15 +0800

    Pola vertikal

--- Day66.java
+++ Day66.java
@@ -1,10 +1,13 @@
 package OneHundredDaysOfCoding;
-
+import java.util.Scanner;
 public class Day66 {
     public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        System.out.print("Tinggi pola: ");
+        int t = i.nextInt();
         char ascii = 65;
-        for (int i = 1; i <= 15; i++) {
-            for (int j = 1; j <= 3; j++) {
+        for (int v = 1; v <= t; v++) {
+            for (int j = 0; j <= (t/5); j++) {
                 if (ascii > 90) ascii = 65;
                 System.out.print(ascii+" ");
                 ascii++;
