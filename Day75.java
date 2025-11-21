235f65226f3f65f651ed79af443876fbbdd2cb73
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-11-21 23:23:08 +0800

    Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ Day75.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day75 {
+    public static void main(String[] args) {
+        String key = "Test Key 123";
+        System.out.println(key.length());
+        System.out.println(key.toUpperCase());
+        System.out.println(key.toLowerCase());
+    }
+}
