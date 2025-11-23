746970263b34ec9a3a4ff063966a7766b56b665c
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-11-23 23:39:11 +0800

    Manipulasi String

---
+++ Day77.java
@@ -0,0 +1,10 @@
+package OneHundredDaysOfCoding;
+public class Day76 {
+    public static void main(String[] args) {
+        String text = " Ini adalah sebuah text ";
+        System.out.println(text.substring(8, 11));
+        System.out.println(text.replace("adalah", "merupakan"));
+        System.out.println(text.replace('a', 'e'));
+        System.out.println(text.trim());
+    }
+}
