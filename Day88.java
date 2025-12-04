7a8f42cad2945187b1ec566dea13778c98ec9e6e
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-04 20:04:56 +0800

    Akses elemen array (For Each)

---
+++ Day88.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day88 {
+    public static void main(String[] args) {
+        String[] a = {"Apel", "Jeruk", "Nanas", "Semangka"};
+        for (String b : a) {
+            System.out.println(b);
+        }
+    }
+}
