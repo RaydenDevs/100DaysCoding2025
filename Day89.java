8eeaee0fffbb29dc12806604f9f393250ce65f87
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-05 21:41:46 +0800

    Method void

--- Day89.java
+++ Day89.java
@@ -1,18 +1,9 @@
 package OneHundredDaysOfCoding;
-import java.util.Scanner;
 public class Day89 {
     public static void main(String[] args) {
-        Scanner i = new Scanner(System.in);
-        System.out.print("Angka Awal\t: ");
-        int awal = i.nextInt();
-        System.out.print("Angka Akhir\t: ");
-        int akhir = i.nextInt();
-        deretanAngka(awal,akhir);
+        say();
     }
-    public static void deretanAngka(int a, int b){
-        System.out.print("Deretan Angka\t: ");
-        for (int i = a; i <= b; i++) {
-            System.out.print(i+" ");
-        }
+    public static void say(){
+        System.out.println("Hello World!");
     }
 }
