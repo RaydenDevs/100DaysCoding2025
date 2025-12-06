0035008420b52ec886703250d2e7f24170eb8377
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-06 22:59:51 +0800

    Method non void

---
+++ Day90.java
@@ -0,0 +1,11 @@
+package OneHundredDaysOfCoding;
+public class Day90 {
+    public static void main(String[] args) {
+        int angka = 7;
+        int hasil = kuadrat(angka);
+        System.out.println("Kuadrat dari " + angka + " adalah " + hasil);
+    }
+    public static int kuadrat(int x) {
+        return x * x;
+    }
+}
