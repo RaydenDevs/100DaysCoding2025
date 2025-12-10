1833a2e8c305896c9ed6129cd24d5d0ced702979
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-10 22:03:55 +0800

    Mencetak method void

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package OneHundredDaysOfCoding;
+public class Day94 {
+    
+    public static void main(String[] args) {
+        printLn();
+    }
+    public static void printLn(){
+        System.out.println("ini isi dari printLn");
+    }
+}
