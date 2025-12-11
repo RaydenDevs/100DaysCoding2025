9620a9a9d7ba70f2c5bcb8d5b1a358f6d8cede4f
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-11 21:09:58 +0800

    Mencetak dengan parameter void

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package OneHundredDaysOfCoding;
+public class Day95 {
+    
+    public static void main(String[] args) {
+        String nama = "Rayhan";
+        halo(nama);
+    }
+    public static void halo(String nama){
+        System.out.println("Halo! Nama saya "+nama);
+    }
+}
