e9cf83cf750300e130b30f1ce46d31e5ad8cd941
Muh.Rayhan.S <139787019+RaydenDevs@users.noreply.github.com>
2025-12-13 21:14:13 +0800

    Method luas persegi

---
+++ Day97.java
@@ -0,0 +1,9 @@
+package OneHundredDaysOfCoding;
+public class Day97 {
+    
+    public static void main(String[] args) {
+        double a = 5;
+        System.out.println(luasPersegi(a));
+    }
+    public static double luasPersegi(double s) {return s *= s;}
+}
